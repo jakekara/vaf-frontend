095530c6655d5b15794a2a7797f78996fe586802
Jake Kara <jake@jakekara.com>
2020-09-24 10:29:55 -0400

    Add SearchArea component

---
+++ src/components/SearchArea/index.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from "react";
+
+export function SearchArea(): ReactElement {
+  return (
+    <div>
+      <input></input>
+      <button type="button">Search</button>
+    </div>
+  );
+}
