6c54afc57f00d9f2681fd608eb711eeeb7dd05f5
Jake Kara <jake@jakekara.com>
2020-10-13 19:09:27 -0400

    Make API endpoint configurable

--- src/api/real/utils/performGet.ts
+++ src/api/real/utils/performGet.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 config();
 
-const END_POINT = "https://or7pwl1efk.execute-api.us-east-2.amazonaws.com/default/api/"
+const END_POINT = process.env.END_POINT || "http://localhost:3030/api/"
 
 export default function performGET<T>(url: string): Promise<Response> {
     return fetch(END_POINT + url).then(resp => resp.json())
