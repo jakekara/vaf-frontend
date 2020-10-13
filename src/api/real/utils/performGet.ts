4c98bc65266220fa4f87ed1afafdd51c26a4055f
Jake Kara <jake@jakekara.com>
2020-10-13 19:21:20 -0400

    Make endpoint configurable

--- src/api/real/utils/performGet.ts
+++ src/api/real/utils/performGet.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 config();
 
-const END_POINT = process.env.END_POINT || "http://localhost:3030/api/"
+const END_POINT = process.env.REACT_APP_ENDPOINT || "http://localhost:3033/api/"
 
 export default function performGET<T>(url: string): Promise<Response> {
     return fetch(END_POINT + url).then(resp => resp.json())
